3375219b8358ff6ad330373b91748eaab395a148
ikreate-innovations <iknnovating@gmail.com>
2025-12-22 23:43:09 +0100

    FIX: Add localStorage cleanup for GDPR compliance

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="light">
       <head>
-        {/* Material Symbols Font */}
+        {/* Material Symbols Font - WE'LL FIX THIS LATER */}
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
@@ -32,6 +32,35 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
       <body className={`${inter.className} bg-background-light text-text-light antialiased`}>
+        {/* LOCALSTORAGE CLEANUP SCRIPT - SILENT VERSION */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                try {
+                  if (typeof window !== 'undefined') {
+                    // Remove Cohere items
+                    localStorage.removeItem('frontChat:store');
+                    localStorage.removeItem('frontChat:status');
+                    localStorage.removeItem('cohereLastSessionId');
+                    localStorage.removeItem('cohereLastWsServerAddr');
+                    localStorage.removeItem('cohere_session_storage_lock');
+                    
+                    // Remove Bugsnag item
+                    localStorage.removeItem('bugsnag-anonymous-id');
+                    
+                    // Remove referrer tracking items
+                    localStorage.removeItem('lastExternalReferrer');
+                    localStorage.removeItem('lastExternalReferrerTime');
+                  }
+                } catch (e) {
+                  // Silent fail - don't show errors to users
+                }
+              })();
+            `,
+          }}
+        />
+        
         <div className="relative flex h-auto min-h-screen w-full flex-col group/design-root overflow-x-hidden">
           <div className="layout-container flex h-full grow flex-col">
             <Header />
